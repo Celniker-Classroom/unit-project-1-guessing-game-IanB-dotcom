9f7f578e18e458a8895132c8c7bf3a70cc25659a
IanB-dotcom <ianakubates@gmail.com>
2026-04-07 20:54:24 +0000

    Asks Name + Allows level selection + Removes buttons after selection

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-// add javascript here
+//Game state
+let answer = 0;
+let guessCount = 0;
+let totalWins = 0;
+let totalGuesses = 0;
+let scores = 0;
+
+//Player Name
+let playerName = prompt("Enter your name:");
+
+document.getElementById("playBtn").addEventListener("click", function(){
+    let radios = document.getElementsByName("level");
+    let range = 3
+    for (let i=0; i < radios.length; i++){
+        if(radios[i].checked){
+            range = parseInt(radios[i].value);
+        }
+    }
+
+
+answer = Math.floor(Math.random() * range) + 1;
+
+document.getElementById("msg").textContent = playerName + ", guess a number between 1 and " + range;
+document.getElemenetById("guess").value ="";
+document.getElemenetById("guessBtn").disabled = false;
+document.getElementById("giveUpBtn").disabled = false;
+document.getElementById("PlayBtn").disabled = true;
+
+let levelRadios = document.getElementsByName("level");
+for (let i = 0; i < levelradios.length; i++) {
+    levelRadios[i].disabled = true;
+}
+
+
+});
